966c9884b4927498b3748cddff6394f2aa37f735
weng3318 <weng7395@gmail.com>
2019-11-15 09:11:27 +0800

    add 1115

--- src/brBookcase.js
+++ src/brBookcase.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bluebird = require('bluebird');
-const brReviewerList = express.Router();
+const brBookcase = express.Router();
 
 const db = mysql.createConnection({
     host:'localhost',
@@ -12,21 +12,32 @@ const db = mysql.createConnection({
 db.connect()
 bluebird.promisifyAll(db)
 
-brReviewerList.get('/:page?', (req, res)=> {
-    
-  
-    db.queryAsync("SELECT COUNT(1) total FROM `br_reviewerlist`"
-    )
+
+const perPage = 9
+brBookcase.get('/:page?/:keyword?', (req, res)=> {
+    // 頁數資料傳輸
+        const output = {};
+        output.params = req.params
+        output.perPage = perPage
+        let page = req.params.page || 1
+
+    // 欄位名稱`total` 取得總筆數
+    let t_sql = 'SELECT COUNT(1) `total` FROM `br_reviewerlist`'
+    db.queryAsync(t_sql)
         .then(results=>{
-            return db.queryAsync("SELECT * FROM `br_reviewerlist` WHERE 1");
-            // return db.queryAsync("SELECT * FROM `br_reviewerlist` LIMIT 0,5");
+            // 拿第一筆[0]名稱total
+            output.totalRows = results[0]['total']
+            output.totalPage = Math.ceil(output.totalRows/perPage)
+            // 判斷用戶頁數範圍
+            if(page<1) page = 1
+            if(page>output.totalPage) page = output.totalPage
+            output.page = page
+
+            return db.queryAsync('SELECT * FROM `br_reviewerlist` LIMIT '+(page-1)*perPage+','+(perPage))
         })
         .then(results=>{
-            // output.rows = results;
-            // res.json(output);
-            // 測試大專資料傳輸
-            res.json(results);
-            // console.log(res.json(results))
+            output.rows=results
+            res.json(output)
         })
         .catch(error=>{
             res.send('404 沒有取得資料！');
@@ -34,4 +45,4 @@ brReviewerList.get('/:page?', (req, res)=> {
         });
   });
 
-module.exports = brReviewerList;
+module.exports = brBookcase;
